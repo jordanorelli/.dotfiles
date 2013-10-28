108beaaaa55de92846f63aa1a0e69b7075b422ed
Jordan Orelli <jorelli@etsy.com>
2013-10-28 21:11:50 +0000

    kinda need this bash profile huh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 BACKUP_DIR="$HOME/.dotfiles_backup"
 BASE_DIR="$HOME/.dotfiles"
-INCLUDE=".bashrc .vimrc .vim"
+INCLUDE=".bashrc .bash_profiles .vimrc .vim"
 
 echo "backing up existing dotfiles into $BACKUP_DIR"
 mkdir -p "$BACKUP_DIR"
