e511b627bc9397a9eeafe25dd627567c1e3fac06
Jordan Orelli <jordanorelli@gmail.com>
2016-06-22 14:02:38 -0400

    clone vundle too

--- install.sh
+++ install.sh
@@ -23,4 +23,8 @@ for FNAME in $INCLUDE; do
 	# symlink in the versioned dotfiles.
 	echo "ln $BASE_DIR/$FNAME" "$HOME/$FNAME"
 	ln -sf "$BASE_DIR/$FNAME" "$HOME/$FNAME"
+
+    # setup Vundle
+    echo "cloning Vundle"
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 done
